2521b8af19caaaecaea5b3d726a5919f7ae782ca
Aditya Uniyal <uniyal2001@gmail.com>
2022-06-21 22:27:35 +0530

    1st upload
    These are upload after completion of login

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
 import './App.css';
+import Login from './Login';
+import { getTokenFromUrl } from "./spotify";
+
 
 function App() {
+  const [token, setToken] = useState(null);// keeping value of token hidden
+
+  //run code based on a given condition
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token){
+      setToken(_token)
+    }
+
+    console.log("I HAVE A TOKEN:> ", token);
+  }, []);
+
+  //if logged in it loads otherwise back to login
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {
+        token? (
+          <h1>I am logged in</h1>
+        ) : (
+          <Login />
+        )
+      }
     </div>
   );
 }
